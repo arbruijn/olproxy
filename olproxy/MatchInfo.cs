f62710bc62a17a797a15da6dc2b7d45d45b13feb
Arne de Bruijn <mail@arnedebruijn.nl>
2019-02-26 18:53:36 +0100

    Added MatchInfo class

---
+++ olproxy/MatchInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using minijson;
+
+namespace olproxy
+{
+    using MJDict = Dictionary<string, object>;
+    using MJList = List<object>;
+
+    class MatchInfo
+    {
+        private string Creator;
+        private string Level;
+        private bool IsTeams;
+        private int PlayerCount;
+        private bool HasMatchData;
+
+        public override string ToString()
+        {
+            return (HasMatchData ? (IsTeams ? "Teams, " : "") + Level + ", by " + Creator + ", " : "") + PlayerCount + " players";
+        }
+
+        public MatchInfo(string matchMessage)
+        {
+            var msg = MiniJson.Parse(matchMessage);
+            var attr = (msg as MJDict)["attr"] as MJDict;
+            if (!attr.TryGetValue("mm_players", out object plVal) && !attr.TryGetValue("mm_mmPlayers", out plVal))
+                return;
+            var players = MiniJson.Parse((plVal as MJDict)["S"] as string) as MJList;
+            PlayerCount = players.Count;
+            foreach (var pl in players)
+                if (((pl as MJDict)["PlayerAttributes"] as MJDict).TryGetValue("private_match_data", out object data))
+                {
+                    var privateMatchData = new PrivateMatchData(System.Convert.FromBase64String((data as MJDict)["valueAttribute"] as string));
+                    HasMatchData = true;
+                    Creator = privateMatchData.Creator;
+                    Level = privateMatchData.LevelName;
+                    IsTeams = privateMatchData.Mode == 1;
+                }
+        }
+    }
+}
