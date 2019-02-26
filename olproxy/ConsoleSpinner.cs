31ce400621f1d36bb13d76b48cc5bd6d8545cc35
Arne de Bruijn <mail@arnedebruijn.nl>
2019-02-26 18:53:18 +0100

    Added console spinner

---
+++ olproxy/ConsoleSpinner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace olproxy
+{
+    class ConsoleSpinner
+    {
+        private static readonly char[] spinChars = new[] { '/', '-', '\\', '|' };
+        private int idx;
+        public DateTime LastUpdateTime;
+        public bool Active;
+
+        public void Spin()
+        {
+            Console.Write(new[] { spinChars[idx], '\r' });
+            idx = (idx + 1) % spinChars.Length;
+            LastUpdateTime = DateTime.UtcNow;
+            Active = true;
+        }
+
+        public void Clear()
+        {
+            Console.Write(" \r");
+            Active = false;
+        }
+    }
+}
