c4e8a857778801f1498b1d31edacf9970a109406
Ronald M. Clifford <roncli@roncli.com>
2019-10-17 22:19:33 -0700

    Remove tracker communication from olproxy.

--- olproxy/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace olproxy {
-    public class AppSettings {
-        public bool isServer { get; set; }
-        public bool signOff { get; set; }
-        public string trackerBaseUrl { get; set; }
-        public string serverName { get; set; }
-        public string notes { get; set; }
-    }
-}
